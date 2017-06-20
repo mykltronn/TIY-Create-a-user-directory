1adf96fecdc53ee9025657c2f444b3312c81c076
Garmhausen <robertgarmhausen@gmail.com>
2017-06-20 18:50:34 -0400

    Fix the mustache.

--- index.js
+++ index.js
@@ -15,35 +15,13 @@ app.set('views', __dirname + '/views');
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  let obj =  [{}];
-  let markup = ``;
-  for (var i = 0; i < data.users.length; i++) {
-    markup += `
-      <a href="${i}"><div class="robot">
-        <img src="${data.users[i].avatar}" />
-        <h2>${data.users[i].username}</h2>
-    `;
-    if (data.users[i].job) {
-      markup += `
-        <p>${data.users[i].job},<br />
-        ${data.users[i].company}</p>
-        </div></a>
-      `;
-    } else {
-      markup += `
-        <p class="unemp">Available for hire</p>
-        </div></a>
-      `
-    }
-  }
-  obj['markup'] = markup;
+  res.render('index.mustache', { users: data.users });
+});
 
-  res.render('index.mustache', obj);
-})
-
-app.get('/:index', function (req, res) {
-  
-})
+app.get('/:indexNum', function (req, res) {
+  let i = parseInt(req.params.indexNum) - 1;
+  res.render('user.mustache', data.users[i]);
+});
 
 app.listen(3000, function () {
   console.log('Successfully started express application!')
