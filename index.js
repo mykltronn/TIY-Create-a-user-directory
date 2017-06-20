a794418dabd009e77d76caa553ef2ad6d8bc31f3
Garmhausen <robertgarmhausen@gmail.com>
2017-06-20 14:30:01 -0400

    Made the thing work.  Not entirely sure how.

--- index.js
+++ index.js
@@ -17,7 +17,29 @@ app.set('views', __dirname + '/views');
 
 
 app.get('/', function (req, res) {
-  res.send("now we're talking...");
+  let obj =  [{}];
+  let markup = ``;
+  for (var i = 0; i < data.users.length; i++) {
+    markup += `
+      <div class="robot">
+        <img src="${data.users[i].avatar}" />
+        <h2>${data.users[i].username}</h2>
+    `;
+    if (data.users[i].job) {
+      markup += `
+        <p>${data.users[i].job}</p>
+        </div>
+      `;
+    } else {
+      markup += `
+        <p class="unemp">Available for hire</p>
+        </div>
+      `
+    }
+  }
+  obj['markup'] = markup;
+
+  res.render('index.mustache', obj);
 })
 
 app.listen(3000, function () {
